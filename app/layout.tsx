0adb4c0b335de6df5e754eb34561a5d2d6897f13
ovidiuteo <ovidiuteo@gmail.com>
2026-04-30 22:52:28 +0300

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
+  icons: {
+    icon: '/favicon.svg',
+  },
   title: 'SetSail — Practică',
   description: 'Platformă de gestionare examene practice SetSail',
 }
